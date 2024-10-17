e06f38276a8c5f6e4bd22e72397a66c816bdb6ec
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2024-10-17 23:00:48 +0500

    completed

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 function page() {
     return (
         <div className='text-3xl p-5'>
-            Home Page
+            <h1 className='font-bold'>Welcome to our website.</h1>
+            <h2>We are committed to providing you with access to our platform. We have made it simple to get started.</h2>
+            <h2>Already a member? Click Login to securely access your account.</h2>
+            <h2>New to our website click Sign in to create an account and unlock all the benefits of our platform.</h2>
         </div>
     );
 }
