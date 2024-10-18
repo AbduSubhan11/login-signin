04549b812cd83e600ba86af1852bbe1a38d9c9d5
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2024-10-18 22:17:14 +0500

    completed

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function page() {
-    return (
-        <div className='text-3xl p-5'>
-            <h1 className='font-bold'>Welcome to our website.</h1>
-            <h2>We are committed to providing you with access to our platform. We have made it simple to get started.</h2>
-            <h2>Already a member? Click Login to securely access your account.</h2>
-            <h2>New to our website click Sign in to create an account and unlock all the benefits of our platform.</h2>
-        </div>
-    );
+  return (
+    <div className="text-3xl p-5 space-y-5">
+      <h1 className="font-bold text-center">Welcome to our website.</h1>
+      <div>
+        <h2>
+          We are committed to providing you with access to our platform. We have
+          made it simple to get started.
+        </h2>
+        <h2>Already a member? Click Login to securely access your account.</h2>
+        <h2>
+          New to our website click Sign in to create an account and unlock all
+          the benefits of our platform.
+        </h2>
+      </div>
+    </div>
+  );
 }
 
-export default  page
+export default page;
